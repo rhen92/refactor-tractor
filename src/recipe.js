42a715e842314693d4aba0bf0a3fac3c08525805
Chris Anderson <mistercanderson@gmail.com>
2021-04-13 19:09:27 -0600

    Write calculateIngredientsCost

--- src/recipe.js
+++ src/recipe.js
@@ -1,15 +1,19 @@
 class Recipe {
-  constructor(recipe) {
+  constructor(recipe, ingredientData) {
     this.id = recipe.id;
     this.name = recipe.name;
     this.image = recipe.image;
     this.tags = recipe.tags;
     this.ingredients = recipe.ingredients;
+    this.ingredientData = ingredientData.filter(data => this.ingredients.some(ingredient => ingredient.id === data.id))
   }
+  
   calculateIngredientsCost() {
-    // return this.ingredients.map(i => {
-    //   ingredientData.find(ingredient => ingredient === i);
-    // });
+    const sortedIngredients = this.ingredients.sort((a, b) => a.id - b.id)
+    const sortedIngredientData = this.ingredientData.sort((a, b) => a.id - b.id)
+    return sortedIngredientData.reduce((counter, ingredient, index) => {
+      return (counter + ((ingredient.estimatedCostInCents / 100) * sortedIngredients[index].quantity.amount))
+    }, 0)
   }
 }
 
