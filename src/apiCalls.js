5be4f59c1d18ea54bafa8a83f5c03be6e2d05689
Angela Battillo <angela.battillo@gmail.com>
2021-04-14 21:30:23 -0600

    Adjust syntax in api call file

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,2 +1,5 @@
 export const userGET = () => fetch('http://localhost:3001/api/v1/users')
   .then(response => response.json()).catch(err => console.log('ERROR'));
+
+export const ingredientsGet = () => fetch('http://localhost:3001/api/v1/ingredients')
+  .then(response => response.json()).catch(err => err.message);
