e9449984d53cb762d0502f7eb2cb40bfcecea73a
Chris Anderson <mistercanderson@gmail.com>
2021-04-16 18:40:41 -0600

    "Weird git glitch from renaming user.js to User.js"

--- src/user.js
+++ src/user.js
@@ -20,13 +20,13 @@ class User {
     this.recipesToCook.push(recipe);
   }
 
-  filterRecipesByTag(tag) {
-    return this.favoriteRecipes.filter(recipe => recipe.tags.includes(tag));
-  }
+  // filterRecipesByTag(tag) {
+  //   return this.favoriteRecipes.filter(recipe => recipe.tags.includes(tag));
+  // }
 
-  searchForRecipeByIngredient(keyword) {
-    return this.favoriteRecipes.filter(recipe => recipe.name.includes(keyword) || recipe.ingredients.includes(keyword));
-  }
+  // searchForRecipeByIngredient(keyword) {
+  //   return this.favoriteRecipes.filter(recipe => recipe.name.includes(keyword) || recipe.ingredients.includes(keyword));
+  // }
 }
 
 module.exports = User;
