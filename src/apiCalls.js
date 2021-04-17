017198769bcdf6b8296d03576420d14d05a1e93a
Chris Anderson <mistercanderson@gmail.com>
2021-04-17 15:30:03 -0600

    Clean up usersGET

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,24 +1,23 @@
 import User from './user';
 
-let users = [];
-let ingredientData = [];
-let recipeData = [];
+let users;
+let ingredientData;
+let recipeData;
 
 const usersGET = () => fetch('http://localhost:3001/api/v1/users')
 .then(response => response.json())
-.then(data => data.map(point => new User(point)))
-.then(user => users.push(user))
-.catch(err => console.log('User Data ERROR'));
+.then(data => users = data.map(point => new User(point)))
+.catch(err => err.message);
 
 const ingredientsGET = () => fetch('http://localhost:3001/api/v1/ingredients')
 .then(response => response.json())
-.then(data => data.forEach(point => ingredientData.push(point)))
-.catch(err => 'Ingredient Data ERROR');
+.then(data => ingredientData = data)
+.catch(err => err.message);
 
 const recipesGET = () => fetch('http://localhost:3001/api/v1/recipes')
 .then(response => response.json())
-.then(data => data.forEach(point => recipeData.push(point)))
-.catch(err => console.log('Recipe Error!'));
+.then(data => recipeData = data)
+.catch(err => err.message);
 
 usersGET();
 ingredientsGET();
